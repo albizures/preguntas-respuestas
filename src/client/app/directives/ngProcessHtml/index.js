8a2e718acf217bdb386e702e65baf4f7067d647e
Jose Albizures <albizures3601@gmail.com>
2016-03-27 11:17:05 -0600

    se arreglo la directiva para procesar el html

--- src/client/app/directives/ngProcessHtml/index.js
+++ src/client/app/directives/ngProcessHtml/index.js
@@ -1,62 +1,59 @@
 'use strict';
 
-function link(scope, element) {
-	scope.termino = function () {
-		let elementos = element.get(0).querySelectorAll('p > span,img'), newFile, file;
-		for (let i = 0; i < elementos.length; i++) {
-			let obj = elementos[i];
-			if (obj.parentElement.tagName == 'SPAN') {
-				if (obj.tagName == 'SPAN') {
-					obj.parentElement.id = obj.parentElement.id || 'P-' + Date.now().toString() + i.toString();
-				} else if (obj.tagName == 'IMG') {
-					obj.parentElement.id = obj.parentElement.id || 'IMG-' + Date.now().toString() + i.toString();
-				}
-			} else {
-				if (obj.tagName == 'SPAN') {
-					obj.parentElement.id = obj.parentElement.id || 'P-' + Date.now().toString() + i.toString();
-				} else if (obj.tagName == 'IMG') {
-					obj.parentElement.id = obj.parentElement.id || 'iMG-' + Date.now().toString() + i.toString();
-				}
-			}
-		}
-		newFile = new File([element.get(0).children[0].innerHTML], 'name.html', {
-			type: "text/html"
-		});
-		file = new FileUploader.FileItem(scope.uploader, newFile);
-		file.progress = 100;
-		file.isUploaded = true;
-		file.isSuccess = true;
-
-		file.formData.push({
-			'nombre_doc': scope.documento.ubicacion,
-			'idEvento': scope.documento.id_evento,
-			'id': scope.documento.id
-		});
-		scope.uploader.queue.push(file);
-		scope.uploader.queue[0].upload();
-	};
-}
-
-function controller($scope) {
-	scope.uploader = new FileUploader({
-		url: 'server/api/uploadFileUPD',
-		headers: 'Content-Type: text/html; charset=UTF-8'
-	});
-	$scope.procesar = function (documento) {
-		console.log(documento);
-		$scope.documento = documento;
-		$scope.$applyAsync();
-	};
-	$scope.addHandlerProcess($scope.procesar);
-}
-
 module.exports = function ngProcessHtml(Data, FileUploader) {
 	return {
 		template: require('./ngProcessHtml.jade'),
 		scope: {
 			'addHandlerProcess': '=ngProcessHtml'
 		},
-		link: link,
-		controller: controller
+		link: function (scope, element) {
+			scope.termino = function () {
+				let elementos = element.get(0).querySelectorAll('p > span,img'),
+					newFile, file;
+				for (let i = 0; i < elementos.length; i++) {
+					let obj = elementos[i];
+					if (obj.parentElement.tagName == 'SPAN') {
+						if (obj.tagName == 'SPAN') {
+							obj.parentElement.id = obj.parentElement.id || 'P-' + Date.now().toString() + i.toString();
+						} else if (obj.tagName == 'IMG') {
+							obj.parentElement.id = obj.parentElement.id || 'IMG-' + Date.now().toString() + i.toString();
+						}
+					} else {
+						if (obj.tagName == 'SPAN') {
+							obj.parentElement.id = obj.parentElement.id || 'P-' + Date.now().toString() + i.toString();
+						} else if (obj.tagName == 'IMG') {
+							obj.parentElement.id = obj.parentElement.id || 'iMG-' + Date.now().toString() + i.toString();
+						}
+					}
+				}
+				newFile = new File([element.get(0).children[0].innerHTML], 'name.html', {
+					type: "text/html"
+				});
+				file = new FileUploader.FileItem(scope.uploader, newFile);
+				file.progress = 100;
+				file.isUploaded = true;
+				file.isSuccess = true;
+
+				file.formData.push({
+					'nombre_doc': scope.documento.ubicacion,
+					'idEvento': scope.documento.id_evento,
+					'id': scope.documento.id
+				});
+				scope.uploader.queue.push(file);
+				scope.uploader.queue[0].upload();
+			};
+		},
+		controller: function ($scope) {
+			$scope.uploader = new FileUploader({
+				url: 'server/api/uploadFileUPD',
+				headers: 'Content-Type: text/html; charset=UTF-8'
+			});
+			$scope.procesar = function (documento) {
+				console.log(documento);
+				$scope.documento = documento;
+				$scope.$applyAsync();
+			};
+			$scope.addHandlerProcess($scope.procesar);
+		}
 	};
 };
